31eeed1075dd9b69d9335c7327f68228385c7a24
Blesswin Samuel <git@bless.win>
2026-04-23 11:54:54 +0530

    test: replace external URL download in benchmark with in-memory test image

--- internal/mediaprocessor/media_processor_test.go
+++ internal/mediaprocessor/media_processor_test.go
@@ -1,10 +1,10 @@
 package mediaprocessor
 
 import (
-	"errors"
-	"io"
-	"net/http"
-	"os"
+	"bytes"
+	"image"
+	"image/color"
+	"image/jpeg"
 	"testing"
 
 	"github.com/davidbyttow/govips/v2/vips"
@@ -71,31 +71,18 @@ func TestParseVipsSize(t *testing.T) {
 	}
 }
 
-func downloadFile(url, fileName string) error {
-	//Get the response bytes from the url
-	response, err := http.Get(url)
-	if err != nil {
-		return err
+func makeTestJPEG(width, height int) []byte {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			img.Set(x, y, color.RGBA{R: uint8(x % 256), G: uint8(y % 256), B: 128, A: 255})
+		}
 	}
-	defer response.Body.Close()
-
-	if response.StatusCode != 200 {
-		return errors.New("Received non 200 response code")
+	var buf bytes.Buffer
+	if err := jpeg.Encode(&buf, img, &jpeg.Options{Quality: 85}); err != nil {
+		panic(err)
 	}
-	//Create a empty file
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	//Write the bytes to the fiel
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return buf.Bytes()
 }
 
 func BenchmarkProcessMetadataRequest(b *testing.B) {
@@ -105,34 +92,13 @@ func BenchmarkProcessMetadataRequest(b *testing.B) {
 		MaxCacheFiles:    0,
 		MaxCacheMem:      50 * 1024 * 1024,
 		MaxCacheSize:     100,
-		// ReportLeaks      :
-		// CacheTrace       :
-		// CollectStats     :
 	})
-	// defer vips.Shutdown()
 
-	// Load test image
-	if err := os.Mkdir("tempdata", 0755); err != nil {
-		if !os.IsExist(err) {
-			b.Fatalf("failed to create tempdata directory: %v", err)
-		}
-	}
-	if _, err := os.Stat("tempdata/christmas-tree.jpg"); os.IsNotExist(err) {
-		err := downloadFile("https://wallpaperswide.com/download/small_christmas_tree-wallpaper-2560x1600.jpg", "tempdata/christmas-tree.jpg")
-		if err != nil {
-			b.Fatalf("failed to download test image: %v", err)
-		}
-	}
-	imageBytes, err := os.ReadFile("tempdata/christmas-tree.jpg")
-	if err != nil {
-		b.Fatalf("failed to read test image: %v", err)
-	}
+	imageBytes := makeTestJPEG(2560, 1600)
 
 	// Create metadata options
 	params := &MetadataOptions{
-		// BlurHash:  true,
 		ThumbHash: true,
-		// PotatoWebp: true,
 	}
 
 	mp := NewMediaProcessor()
